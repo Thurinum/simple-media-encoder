d17b43a24e4142a2c4b7b1001e183888f205eeea
William Boulanger <willbou2@gmail.com>
2024-12-24 16:06:43 -0500

    Nix package with dev shell integration

--- flake.nix
+++ flake.nix
@@ -1,34 +1,49 @@
 {
   description = "Dev environment for simple-media-encoder";
 
-  inputs.devshell.url = "github:numtide/devshell";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, flake-utils, devshell, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system: {
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      packages = rec {
+        SimpleMediaEncoder = pkgs.qt6Packages.callPackage ./build.nix {};
+        default = SimpleMediaEncoder;
+      };
       devShell =
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ devshell.overlays.default ];
-          };
-        in
-        pkgs.devshell.mkShell {
-            packages = with pkgs; [
-              lldb
+        pkgs.mkShell {
+          inputsFrom = [
+            self.packages.${system}.default
+          ];
+          buildInputs = with pkgs; [
+            lldb
+            clang
+            cmake
 
-              # LSPs for Emacs or Neovim
-              cmake-language-server
+            # LSPs for Emacs or Neovim
+            python3Minimal
+            cmake-language-server
+            clang-tools
 
-              # build dependencies
-              cmake
-              gnumake
-              clang
-              qt6.full
+            qt6.wrapQtAppsHook
+            qt6.qtbase
+            makeWrapper
 
-              # runtime dependencies
-              ffmpeg
-            ];
+            # runtime deps
+            ffmpeg
+          ];
+          shellHook = ''
+            fishdir=$(mktemp -d)
+            makeWrapper "$(type -p fish)" "$fishdir/fish" "''${qtWrapperArgs[@]}"
+
+            export CXX="clang++"
+            export CC="clang"
+
+            exec "$fishdir/fish"
+        '';
         };
     });
 }
